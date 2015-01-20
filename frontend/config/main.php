c85ce3935af857b9ef2efc0989b2056b79b48db8
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-01-20 13:07:15 +0100

    Added kartik gridview & datecontrol modules

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,9 +1,8 @@
 <?php
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require(__DIR__.'/../../common/config/params.php'),
+    require(__DIR__.'/../../common/config/params-local.php'),
+    require(__DIR__.'/params.php'), require(__DIR__.'/params-local.php')
 );
 
 return [
@@ -28,6 +27,28 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+    ],
+    'modules' => [
+        'gridview' => [
+            'class' => 'kartik\grid\Module',
+        ],
+        'datecontrol' => [
+            'class' => 'kartik\datecontrol\Module',
+            // format settings for displaying each date attribute
+            'displaySettings' => [
+                'date' => 'd-m-Y',
+                'time' => 'H:i:s A',
+                'datetime' => 'd-m-Y H:i:s A',
+            ],
+            // format settings for saving each date attribute
+            'saveSettings' => [
+                'date' => 'Y-m-d',
+                'time' => 'H:i:s',
+                'datetime' => 'Y-m-d H:i:s',
+            ],
+            // automatically use kartik\widgets for each of the above formats
+            'autoWidget' => true,
+        ]
     ],
     'params' => $params,
 ];
