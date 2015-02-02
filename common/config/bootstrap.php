046920a78e801da709a3dc986f512146f977e571
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-02-02 09:14:13 +0100

    Dependency management added

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('frontend', dirname(dirname(__DIR__)).'/frontend');
+Yii::setAlias('backend', dirname(dirname(__DIR__)).'/backend');
+Yii::setAlias('console', dirname(dirname(__DIR__)).'/console');
+// Own aliases
+Yii::setAlias('jk', dirname(dirname(__DIR__)).'/common/lib/jk');
+
+// Register dependency
+\jk\sys\DependencyManager::register(
+    yii\helpers\ArrayHelper::merge(
+        require(__DIR__.'/dependency.php'),
+        require(__DIR__.'/dependency-local.php')
+));
