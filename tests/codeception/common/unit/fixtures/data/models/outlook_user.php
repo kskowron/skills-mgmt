34ce2cbcdb262d0a34a188a77e8cf735b58de595
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-02-02 09:12:54 +0100

    Various test for application

---
+++ tests/codeception/common/unit/fixtures/data/models/outlook_user.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    [
+        'username' => 'o_user',
+        'auth_key' => 'X',
+        //password_0
+        'password_hash' => 'X',
+        'password_reset_token' => 'X',
+        'created_at' => '1402312317',
+        'updated_at' => '1402312317',
+        'email' => 'email@email.com',
+    ],
+];
