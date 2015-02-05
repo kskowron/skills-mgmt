d9d7f14c271ddefbfe0fd3021c1753b7d6ef05d8
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-02-05 12:19:37 +0100

    refactoring into MyProfile

---
+++ frontend/controllers/MyProfileController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Employee;
+use common\models\EmployeeSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * ProfileController implements the CRUD actions for Employee model.
+ */
+class MyProfileController extends Controller
+{
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Displays a single Employee model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView()
+    {
+        if (($model = Employee::findOne(['user_id'=>\Yii::$app->user->id])) == NULL) {
+            $model = new Employee();
+        };
+        if ($model->load(Yii::$app->request->post())){
+            $model->user_id = \Yii::$app->user->id;
+            if($model->save()){
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        }
+        return $this->render('view', ['model' => $model]);
+    }
+
+    /**
+     * Creates a new Employee model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Employee;
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create',              [
+                    'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Employee model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update',
+                    [
+                    'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Employee model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Employee model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Employee the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Employee::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
