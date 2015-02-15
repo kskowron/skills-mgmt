7e37cdaa3d45731beb6f7b5abcc1f48dfc7cb5bc
Ksawery Skowron <ksawery@192.168.0.20>
2015-02-15 10:32:58 +0100

    Regards #43. ListView used to display people of selected business profile instead of GridView.

--- frontend/controllers/BusinessProfileController.php
+++ frontend/controllers/BusinessProfileController.php
@@ -5,33 +5,62 @@ namespace frontend\controllers;
 use common\models\BusinessProfile;
 use common\models\BusinessProfileSearch;
 use frontend\models\EmployeeSearchExt;
+use frontend\models\SearchBusinessProfileForm;
 use Yii;
+use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
 class BusinessProfileController extends Controller {
 
-    public function actionIndex() {
-        $businessProfileSearch = new BusinessProfileSearch();
-        $dataProvider = $businessProfileSearch->search(NULL);
-        return $this->render('index', ['dataProvider' => $dataProvider]);
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['view'],
+                'rules' => [
+                    [
+                        'actions' => ['view'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
     }
 
     public function actionView() {
+        Yii::$app->session->set('profileBackUrl', Yii::$app->request->getAbsoluteUrl());
         $params = Yii::$app->request->getQueryParams();
-        if (ArrayHelper::getValue($params, 'id', NULL) == NULL) {
-            $this->redirect(['business-profile/index']);
+        $form = new SearchBusinessProfileForm();
+        
+        if(ArrayHelper::keyExists($form->formName(), $params)) {
+            $form->load($params);
+            $form->validate();
+            $formParams = ArrayHelper::remove($params, $form->formName());
+            $searchParams = ArrayHelper::merge($params, $formParams);
         } else {
-            $businessProfile = BusinessProfile::findOne($params['id']);
-            if ($businessProfile == NULL) {
-                throw new NotFoundHttpException(Yii::t('skills', Yii::t('skills', 'The requested business profile does not exist.')));
-            }
-            $employeesSearch = new EmployeeSearchExt();
-            $dataProvider = $employeesSearch->searchByBusinessProfile($params);
+            $searchParams = $params;
+            $form->id = ArrayHelper::getValue($searchParams, 'id', NULL);
+        }
+        
+        $profileSearch = new BusinessProfileSearch();
+        $profiles = ArrayHelper::map($profileSearch->search($searchParams)->getModels(), 'id', 'name');
+
+        $businessProfile = BusinessProfile::findOne($searchParams['id']);
+        if ($businessProfile == NULL) {
+            throw new NotFoundHttpException(Yii::t('skills', 'The requested business profile does not exist.'));
         }
 
-        return $this->render('view', ['businessProfile' => $businessProfile, 'dataProvider' => $dataProvider, 'searchModel' => $employeesSearch]);
+        $employeesSearch = new EmployeeSearchExt();
+        $dataProvider = $employeesSearch->searchByBusinessProfile($searchParams);
+        
+        return $this->render('view', ['businessProfile' => $businessProfile, 
+                                       'profiles' => $profiles, 
+                                       'model' => $form,
+                                       'dataProvider' => $dataProvider
+                                      ]);
     }
 
 }
