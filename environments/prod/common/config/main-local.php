d17aa854317f90b07b2bd15dd92e0482a3a4e170
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-02-24 13:45:50 +0100

    fixed local configuration files

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,13 +4,17 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=mysqldb;dbname=skills',
-            'username' => 'root',
+            'username' => 'admin',
             'password' => 'password',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+        ],
+        'mongodb' => [
+            'class' => '\yii\mongodb\Connection',
+            'dsn' => 'mongodb://developer:password@mongodb:27017/skills',
         ],
     ],
 ];
