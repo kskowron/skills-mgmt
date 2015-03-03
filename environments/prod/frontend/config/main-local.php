8ef52b3a0965dcdc89a075d9f063483352e39dbb
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-03-03 16:22:08 +0100

    fixed prod env app name

--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'components' => [
+        'name'=>'Skills-mgmt',
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
