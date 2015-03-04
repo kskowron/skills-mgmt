793ef9e6657bc5b91b2065729d30cb500ab3079c
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-03-04 11:43:49 +0100

    added test for my business profile

---
+++ tests/codeception/frontend/functional/MyBusinessProfile1Cept.php
@@ -0,0 +1,21 @@
+<?php
+
+use tests\codeception\common\_pages\LoginPage;
+use tests\codeception\frontend\FunctionalTester;
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('ensure that my business profile works - emp1');
+
+$loginPage = LoginPage::openBy($I);
+$loginPage->login('emp1', 'test123');
+$I->seeLink('Logout (emp1)');
+
+$I->see('Edit Business Profile', 'a');
+$I->click('Edit Business Profile');
+
+$I->see('BPROFILE1', 'td');
+$I->see('BPROFILE2', 'td');
+$I->see('BPROFILE3', 'td');
+
+$I->dontSee('BPROFILE4', 'td');
+$I->dontSee('BPROFILE5', 'td');
