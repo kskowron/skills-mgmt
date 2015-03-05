f076ac14d56eb01bff9882f295b9cd0f35442605
Jaroslaw Kozak <jaroslaw.kozak68@gmail.com>
2015-03-05 15:49:49 +0100

    refactored code and fix #59

--- frontend/models/AvailableSkillsSearch.php
+++ frontend/models/AvailableSkillsSearch.php
@@ -7,9 +7,7 @@
 
 namespace frontend\models;
 
-use common\models\Employee;
 use common\models\SkillSearch;
-use Yii;
 use yii\data\ActiveDataProvider;
 use yii\helpers\ArrayHelper;
 
@@ -17,20 +15,17 @@ use yii\helpers\ArrayHelper;
  * Description of AvailableSkillsSearch
  *
  * @property string $category_name Name of skills category
- * @author jarek
+ * @author Jarosław Kozak <jaroslaw.kozak68@gmail.com>
+ *
+ * @property int $for_employee_id wor whom search will be done
+ * @property string $category_name
+ *
  */
 class AvailableSkillsSearch extends SkillSearch
 {
 
-    public $loggedEmployee;
-
+    protected $for_employee_id;
     protected $category_name;
-
-    public function init()
-    {
-        parent::init();
-        $this->loggedEmployee = Employee::findOne(['user_id' => Yii::$app->user->id]);
-    }
 
     public function rules()
     {
@@ -40,7 +35,7 @@ class AvailableSkillsSearch extends SkillSearch
 
     public function search($params)
     {
-        $query = self::getUnassignedSkillsQuery($this->loggedEmployee->id);
+        $query = self::getUnassignedSkillsQuery($this->for_employee_id);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
@@ -80,7 +75,17 @@ class AvailableSkillsSearch extends SkillSearch
 
     function setCategory_name($category_name)
     {
-        $this->catetory_name = $catetory_name;
+        $this->category_name = $category_name;
+    }
+
+    function getFor_employee_id()
+    {
+        return $this->for_employee_id;
+    }
+
+    function setFor_employee_id($for_employee_id)
+    {
+        $this->for_employee_id = $for_employee_id;
     }
 
 }
